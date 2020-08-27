7ced767f9d096a5cd8d9b9f2199337cd41c1809a
pechenika <eugene3009@gmail.com>
2020-08-27 20:12:17 +0700

    Modal layout fixed + empty desc validation

--- frontend/src/components/TaskListWidget/components/TaskListPage/EditTaskModal.tsx
+++ frontend/src/components/TaskListWidget/components/TaskListPage/EditTaskModal.tsx
@@ -7,8 +7,10 @@ import {
     setTasks,
     setEditingTask
 } from "./TaskListLogic"
-import { editTask, findAllTasks } from '../../../../api/Rest'
+import { editTask } from '../../../../api/Rest'
 import _ from 'lodash'
+import './TaskListPage.css'
+import { Modal } from 'react-bootstrap'
 
 const EditTaskModal: React.FunctionComponent<{}> = () => {
     const dispatch = useDispatch()
@@ -24,6 +26,11 @@ const EditTaskModal: React.FunctionComponent<{}> = () => {
     }
 
     const onSubmitEditionClick = async () => {
+        if (!editingTask.description.trim()) {
+            alert("Description cannot be empty!")
+            return
+        }
+
         const value: TaskModel[] = await editTask(editingTask)
         dispatch(clearEditingTask())
         dispatch(setTasks(value))
@@ -34,18 +41,22 @@ const EditTaskModal: React.FunctionComponent<{}> = () => {
     }
 
     return (
-        <div className={'taskList-createTaskModal'}>
-            <input
-                value={editingTask.description}
-                onChange={handleDescriptionChange}
-            />
-            <button onClick={onSubmitEditionClick}>
-                Save
-            </button>
-            <button onClick={onCancelEditionClick}>
-                Cancel
-            </button>
-        </div>
+        <Modal className="edit-task-modal" bsSize="lg" show={!!editingTask} onHide={onCancelEditionClick}>
+            <Modal.Header className="modal-header" closeButton={true}>Edit task</Modal.Header>
+            <Modal.Body className="modal-body">
+                <span className="modal-body-desc-label">Description:</span>
+                <div className="modal-body-desc-input">
+                    <input
+                        value={editingTask.description}
+                        onChange={handleDescriptionChange}
+                    />
+                </div>
+            </Modal.Body>
+            <Modal.Footer className="modal-footer">
+                <button className="button save" onClick={onSubmitEditionClick}>Save</button>
+                <button className="button cancel" onClick={onCancelEditionClick}>Cancel</button>
+            </Modal.Footer>
+        </Modal >
     )
 }
 
